ae2f4a3de04eb76ddd236c1e99994b37bfc1b1d5
María Paz Puerta Acevedo <mariapazpuerta@gmail.com>
2025-03-22 16:48:57 -0500

    Se agregó la documentación del código de la clase DirectorioAmigo

--- src/autonoma/DirectorioAmistadesApp/models/DirectorioAmigo.java
+++ src/autonoma/DirectorioAmistadesApp/models/DirectorioAmigo.java
@@ -9,26 +9,46 @@ import java.util.ArrayList;
 /**
  *
  * @author Maria Paz Puerta
+ * @since 20250319
+ * @version 1.0.0
  */
 public class DirectorioAmigo {
+    /// Atributos
+    /**
+     * Lista para almacenar un amigo y su información
+     */
     private  ArrayList<Amigo> amigos;
-
+    
     public DirectorioAmigo(ArrayList<Amigo> amigos) {
         this.amigos = amigos;
     }
-
+    
+    /**
+     * Obtiene la lista de amigos almacenada en la clase DirectorioAmigo
+     * @return Retorna una lista de tipo Amigo donde se almacenan amigos.
+     */
     public ArrayList<Amigo> getAmigos() {
         return amigos;
     }
-
+    
+    /**
+     * Establece la lista de amigos en el DirectorioAmigo.
+     * @param amigos Lista de objetos tipo Amigo que reemplazará la lista actual.
+     */
     public void setAmigos(ArrayList<Amigo> amigos) {
         this.amigos = amigos;
     }
     
-    
+    /**
+     * Agrega un nuevo amigo al directorio si su nombre no está registrado previamente.
+     * @param nombre El nombre de el amigo a agregar.
+     * @param telefono El teléfono de el amigo a agregar.
+     * @param correoElectronico El correo electrónico de el amigo a agregar.
+     * @return Retorna true si el amigo no existe en la lista y retorna false si ya existe un mismo nombre.
+     */
     public boolean agregarAmigo(String nombre, long telefono, String correoElectronico){
         for (int i = 0; i < amigos.size(); i++) {
-            if (amigos.get(i).getCorreoElectronico().equalsIgnoreCase(correoElectronico)) {
+            if (amigos.get(i).getNombre().equalsIgnoreCase(nombre)) {
                 return false;
             }
         }
@@ -37,6 +57,12 @@ public class DirectorioAmigo {
         return true;
     }
     
+    /**
+     * Busca a un amigo por medio de su correo electrónco en la lista.
+     * @param correoElectronico El correo electrónico de el amigo que desea buscar.
+     * @return Retorna a el amigo si se encontro a la persona por medio de su correo electrónco y retorna null si no se
+     * encontró.
+     */
     public Amigo buscarAmigo(String correoElectronico){
         for (int i = 0; i < amigos.size(); i++) {
             if (amigos.get(i).getCorreoElectronico().equalsIgnoreCase(correoElectronico)) {
@@ -46,6 +72,11 @@ public class DirectorioAmigo {
         return null; 
     }
     
+    /**
+     * Proporciona información sobre la aplicación, incluyendo los nombres de los desarrolladores,
+     * la universidad y la asignatura en la que se desarrolló.
+     * @return Retorna una cadena de texto con la información sobre la aplicación.
+     */
     public String mostrarInformacionAplicacion() {
         String nombreDesarrollador = "María Paz Puerta y Heily Yohana Rios";
         String universidad = "Universidad Autónoma de Manizales";
