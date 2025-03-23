de996fb6c3405268d0c4cc42296a1148614a0f7b
Heily Rios <heilyayala005@gmail.com>
2025-03-22 21:36:04 -0500

    Se agregaron excepciones nuevas

---
+++ src/autonoma/DirectorioAmistadesApp/exceptions/EspaciosEnBlancoException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.DirectorioAmistadesApp.exceptions;
+
+/**
+ *
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ */
+public class EspaciosEnBlancoException extends Exception{
+
+    public EspaciosEnBlancoException() {
+        super("No se pueden registrar solo espacios");
+    }
+    
+    
+}
